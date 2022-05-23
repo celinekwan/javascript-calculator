28145162ac5dde37db70e6f0747ff35b6000fee6
Celine Kwan <celine.kwan2017@gmail.com>
2022-05-23 12:00:32 -0400

    Add addition of two, and multiple numbers functionality

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ const divide = (num1, num2) => {
 }
 
 const operator = (symbol,num1,num2) => {
-    num1 = Number(num1);
-    num2 = Number(num2);
+    // num1 = Number(num1);
+    // num2 = Number(num2);
     switch(symbol) {
         case "+":
             return add(num1,num2);
@@ -26,8 +26,90 @@ const operator = (symbol,num1,num2) => {
     }
 }
 
-let sign = prompt("Operator: ");
-let num1 = prompt("Num 1: ");
-let num2 = prompt("Num 2: ");
-console.log(operator(sign,num1,num2));
-// console.log(divide(Number(num1),Number(num2)));
+const display = document.querySelector('.display'); 
+let num1 = 0;
+let sign = '';
+let num2 = 0;
+let result = 0;
+
+const clearDisplay = () => {
+    display.innerText = '';
+}
+const getDispNum = () => {
+    return Number(display.innerText);
+}
+const resetVars = () => {
+    num1 = 0;
+    sign = '';
+    num2 = 0;
+    result = 0;
+}
+
+document.addEventListener('click', function(event) {
+    if (event.target.matches('.all-clear')) {
+        clearDisplay();
+        resetVars();
+    }
+    if (event.target.matches('#add')) {
+        // 2 args, 1 operator (e.g. (0+)1+1 = 2)
+        if (num1 === 0) {
+            num1 = getDispNum();
+            sign = "+";
+            clearDisplay();
+        }
+        // ------------------
+        // 3 args, 2 operators (e.g. 1+1+1 = 3 )
+        if (num1 !== 0) {
+            num2 = getDispNum();
+            num1 = operator(sign,num1,num2);
+            clearDisplay();
+        }
+        // ------------------
+        sign = "+";
+    }
+    if (event.target.matches("#equals")) {
+        // 2 args, 1 operator (e.g. 1+1 = 2)
+        num2 = getDispNum();
+        
+        // ------------------
+        // 3 args, 2 operators (e.g. 1+1+1 = 3 )
+            // num2 = getDispNum();
+            // clearDisplay();
+        // ------------------
+        result = operator(sign,num1,num2);
+        clearDisplay();
+
+        console.log(result);
+        
+        resetVars();
+    }
+
+
+    if (event.target.matches('#b1')) {
+        display.innerText += '1';
+    }
+    if (event.target.matches('#b2')) {
+        display.innerText += '2';
+    }
+    if (event.target.matches('#b3')) {
+        display.innerText += '3';
+    }
+    if (event.target.matches('#b4')) {
+        display.innerText += '4';
+    }
+    if (event.target.matches('#b5')) {
+        display.innerText += '5';
+    }
+    if (event.target.matches('#b6')) {
+        display.innerText += '6';
+    }
+    if (event.target.matches('#b7')) {
+        display.innerText += '7';
+    }
+    if (event.target.matches('#b8')) {
+        display.innerText += '8';
+    }
+    if (event.target.matches('#b9')) {
+        display.innerText += '9';
+    }
+}, false); 
